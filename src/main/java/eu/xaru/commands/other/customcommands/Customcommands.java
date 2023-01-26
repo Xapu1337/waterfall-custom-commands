2be98077e08633e4951efd193dadbd90d11bae28
Xapu1337 <xapursplaz@gmail.com>
2023-01-26 12:00:36 +0100

    initial commit: added base waterfall plugin

---
+++ src/main/java/eu/xaru/commands/other/customcommands/Customcommands.java
@@ -0,0 +1,16 @@
+package eu.xaru.commands.other.customcommands;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class Customcommands extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
